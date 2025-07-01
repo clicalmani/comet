c504992839acfabafe60e82cd2d868647ad6615c
@clicalmani <honorable85@gmail.com>
2025-07-01 15:14:58 +0100

    fix: Enable Inertia middleware in application setup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,7 +9,8 @@
 use Clicalmani\Foundation\Http\Middlewares\Middleware;
 
 return Clicalmani\Foundation\Maker\Application::setup(rootPath: dirname(__DIR__))
+            ->withService('inertia', [\Inertia\Response::class])
             ->withMiddleware(function(Middleware $middleware) {
-                // $middleware->web(append: [\Inertia\Middleware::class]);
+                $middleware->web(append: [\Inertia\Middleware::class]);
             })
             ->run();
